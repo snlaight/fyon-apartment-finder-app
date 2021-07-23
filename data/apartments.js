6c30d4c0887edc2fc938b85a75675ac656c35c93
Santiago Laight <santilaight@Santiagos-MacBook-Pro.local>
2021-07-23 10:10:09 +0200

    adding tailwind config

---
+++ data/apartments.js
@@ -0,0 +1,110 @@
+export const Apartments = [
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1367,
+    "size": 46,
+    "location": "Rutland Road, Springhill"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1368,
+    "size": 69,
+    "location": "Clinton Avenue, Norfolk"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1198,
+    "size": 66,
+    "location": "Box Street, Grazierville"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1432,
+    "size": 65,
+    "location": "Gerald Court, Northchase"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 285,
+    "size": 57,
+    "location": "Bainbridge Street, Kersey"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1338,
+    "size": 77,
+    "location": "Dean Street, Crenshaw"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 741,
+    "size": 74,
+    "location": "Ditmas Avenue, Reinerton"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 918,
+    "size": 77,
+    "location": "Vandervoort Avenue, Blue"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 485,
+    "size": 97,
+    "location": "Williams Court, Wacissa"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 961,
+    "size": 98,
+    "location": "Harrison Place, Shelby"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1500,
+    "size": 34,
+    "location": "Seigel Street, Coyote"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 821,
+    "size": 78,
+    "location": "Cook Street, Groveville"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 267,
+    "size": 34,
+    "location": "Estate Road, Falconaire"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 765,
+    "size": 88,
+    "location": "Roosevelt Place, Stollings"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 886,
+    "size": 58,
+    "location": "Grattan Street, Cliff"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 971,
+    "size": 63,
+    "location": "Fuller Place, Bowmansville"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 224,
+    "size": 50,
+    "location": "Prospect Avenue, Glendale"
+  },
+  {
+    "picture": "http://placehold.it/32x32",
+    "rent": 1135,
+    "size": 75,
+    "location": "Benson Avenue, Moquino"
+  }
+]
