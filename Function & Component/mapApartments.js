6f87c0b25e9fa315ebb549633651a2b087254042
Santiago Laight <santilaight@Santiagos-MacBook-Pro.local>
2021-07-23 16:09:25 +0200

    adding random image generator for each apartment

--- Function & Component/mapApartments.js
+++ Function & Component/mapApartments.js
@@ -1,13 +1,17 @@
 import { Apartments } from "../data/apartments";
+import styles from "../styles/Home.module.css";
 
 export const mapApartments = () => {
+  const randomNumber = (min,max)=>{
+    min = Math.ceil(min)
+    max = Math.ceil(max)
+    return Math.floor(Math.random()* (max, min +1)) + min
+}
   for (let i = 0; i < Apartments.length; i++) {
-    let rent = Apartments[i].rent;
-    let size = Apartments[i].size;
-    let location = Apartments[i].location;
     const allApartments = Apartments.map((apartment) => {
       return (
         <div className="rounded border-gray-600  dark:border-gray-700 border-dashed border-2 p-2">
+          <img className={styles.apartmentImage} src={`https://source.unsplash.com/collection/1118894/${randomNumber(10,99)}`}/>
           <p>Rent:$ {apartment.rent}</p>
           <p>Location: {apartment.location}</p>
           <p>Size: {apartment.size} sq.M</p>
